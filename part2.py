78a327534331b09ee546dc7eef78ffcc4c1d01af
Nicholas Freeman <93940195+monkeylicker@users.noreply.github.com>
2022-01-07 17:27:22 +0000

    done all 4 parts

--- part2.py
+++ part2.py
@@ -7,4 +7,11 @@ Write a program that asks the user to enter a positive integer n. The program wi
 For example, if the user types in 4, the program should print 100 (since 1^3 + 2^3 + 3^3 + 4^3 = 100).
 """
 
-#write your code here
+n = int(input("Enter a positive integer: "))
+product = 0
+cubed = 1
+for i in range(1, n + 1):
+  cubed = i ** 3
+  product = product + cubed
+
+print(product)
